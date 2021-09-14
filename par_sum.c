fedf2fe53a681fe01d0936513a30ab37af647218
Edson Cizeski <cizeskiedson@gmail.com>
2021-09-14 17:48:21 -0300

    maior parte construida, hora dos testes

--- par_sum.c
+++ par_sum.c
@@ -16,25 +16,26 @@ para ler tarefas de um arquivo e executa-las
 #include <limits.h>
 #include <pthread.h>
 #include <stdbool.h>
+#include <time.h>
 
 
 /* STRUCTS */
-typedef struct tipo_tarefa{
+struct tarefa{
+    int id;
     char comando;
     short int tempo;
-    struct tipo_tarefa *prox;
-}tarefa;
+    struct tarefa *prox;
+};
 
 typedef struct{   
   pthread_t thread_id;        
   int       thread_num;
-  bool      ocioso;
 }thread_info;
 
 /* CONSTANTES GLOBAIS */
 #define MESTRE 0
 #define END_LINE '\n'
-#define PAUSA 'c'
+#define PAUSA 'e'
 
 /* VARIAVEIS GLOBAIS */
 int threads = 0;
@@ -45,26 +46,42 @@ long impar = 0;
 long minimo = INT_MAX;
 long maximo = INT_MIN; 
 
-tarefa *cabeca;
+struct tarefa *cabeca;
 char **buffer;
+bool acabou_programa = false;
+bool ativo = false;
+
+/* MUTEX LOCKS */
+pthread_mutex_t lock_trabalhador;
+pthread_mutex_t lock_mestre;
+pthread_mutex_t lock_fila;
+pthread_cond_t cond_mestre;
+pthread_cond_t cond_trabalhador;
 
 /* CABECALHO DE FUNCOES */
 void le_arquivo(char* filename);
-static void *mestre_trabalhador(void *arg);
-int le_buffer();
+struct tarefa* le_tarefa(int i);
+static void *mestre();
+static void *trabalhador();
 int calcula_tamanho_arq(FILE* arq);
 void inicializa_buffer();
+void do_stuff(int tempo);
+int adiciona(struct tarefa *no);
+void mostra_tarefas();
+void remove_comando(char comando);
 
 /* FLUXO PRINCIPAL */
 int main(int argc, char *argv[]){
+    printf("Bem Vindo\n");
     int opt, tnum, s;
     char *filename;
     thread_info *tinfo;
-    pthread_attr_t attr;
-    void *res;
     
-    cabeca = (tarefa*) malloc(sizeof(tarefa*));
-
+    pthread_mutex_init(&lock_trabalhador, NULL);
+    pthread_mutex_init(&lock_mestre, NULL);
+    pthread_mutex_init(&lock_fila, NULL);
+    cabeca = (struct tarefa*) malloc(sizeof(struct tarefa));
+    cabeca->prox = NULL;
     while ((opt = getopt(argc, argv, "f:t:")) != -1) {
         switch (opt) {
         case 'f': /*Nome do arquivo*/
@@ -79,42 +96,29 @@ int main(int argc, char *argv[]){
         return -1;
         }
     }
-
+    printf("Comecando a leitura do arquivo %s com %d\n", filename, threads);
     le_arquivo(filename);
-
-    s = pthread_attr_init(&attr);
-    if (s != 0)
-       // handle_error_en(s, "pthread_attr_init");
-
-    tinfo = calloc(threads, sizeof(thread_info));
-    if (tinfo == NULL)
-        //handle_error("calloc");
-
-    for (tnum = 0; tnum < threads; tnum++) {
-        tinfo[tnum].thread_num = tnum;
-        if(tnum = 0)
-            tinfo[tnum].ocioso = false;
-        else
-            tinfo[tnum].ocioso = true;
-        s = pthread_create(&tinfo[tnum].thread_id, &attr,
-                &mestre_trabalhador, &tinfo[tnum]);
+    printf("Encerrando a leitura\n");
+    tnum = 0; 
+    pthread_t thread_mestre;
+    pthread_t thread_trabalhador[threads-1];
+    printf("Paralelizando...\n");
+    s = pthread_create(&thread_mestre, NULL, &mestre, NULL);
+    for (tnum = 0; tnum < threads - 1; tnum++) {
+        s = pthread_create(&thread_trabalhador[tnum], NULL, &trabalhador, NULL);
         if (s != 0)
             printf("teste");
              //  handle_error_en(s, "pthread_create");
     }
 
-
-    s = pthread_attr_destroy(&attr);
-    if (s != 0)
-      //  handle_error_en(s, "pthread_attr_destroy");
-
-    for (tnum = 0; tnum < threads; tnum++) {
-        s = pthread_join(tinfo[tnum].thread_id, &res);
+    s = pthread_join(thread_mestre, NULL);
+    for (tnum = 0; tnum < threads - 1; tnum++) {
+        s = pthread_join(thread_trabalhador[tnum], NULL);
         if (s != 0)
+            printf("join");
           //  handle_error_en(s, "pthread_join");
-        free(res);
     }
-    free(tinfo);
+    printf("Encerrando programa...\n");
     exit(EXIT_SUCCESS);
 }
 
@@ -145,6 +149,131 @@ void le_arquivo(char* filename){
     fclose(arq);
 }
 
+/* LE BUFFER PARA A FILA DE TAREFAS */
+static void *mestre(){
+    char *cmd, *tmp;
+    bool acabou_leitura = false;
+    int i =  0;
+    printf("MESTRE\n");
+    do{
+        struct tarefa *no;
+        printf("%d EXECUCAO\n", i);
+        if(i >= n){
+            acabou_leitura = true;
+        }else{
+            no = le_tarefa(i);
+            if(no->comando == PAUSA){
+                printf("Pausa...\n");
+                sleep(no->tempo);
+                i+=1;
+                printf("Saindo da pausa...\n");
+                continue;
+            }
+        }
+        pthread_mutex_lock(&lock_mestre);
+        if(!acabou_leitura){
+            adiciona(no);
+        }
+        mostra_tarefas();
+        while(cabeca->prox == NULL && acabou_leitura){ 
+            printf("acabou programa?\n");
+            //quando a fila ficar vazia espera os trabalhadores ativos terminarem
+            if(ativo){
+                pthread_cond_wait(&cond_mestre, &lock_mestre);
+            }
+            printf("ACABOU\n");
+            acabou_programa = true;
+            break;
+        }
+        pthread_mutex_unlock(&lock_mestre);
+        printf("Executou mestre\n");
+        pthread_cond_signal(&cond_trabalhador); //acordar trabalhadores ociosos
+        i+=1;
+    }while(!acabou_programa); //espera a fila acabar ativando as threads conforme precisar
+    printf("Terminou mestre\n");
+    return 0;
+} 
+/* EXECUTA TAREFAS */
+static void *trabalhador(){
+    printf("TRABALHADOR\n");
+    while(1){
+        struct tarefa *no, *aux;
+        pthread_mutex_lock(&lock_trabalhador);
+        printf("Comecando a executar...\n");
+        ativo = true;
+        while(cabeca->prox == NULL){ //senao houver mais tarefas disponiveis bloquear
+            printf("Trabalhador Ocioso...\n");
+            ativo = false;
+            pthread_cond_wait(&cond_trabalhador, &lock_trabalhador);
+            ativo = true;
+            if(acabou_programa){
+               return 0;
+            }
+        } //se chegou aqui, pode puxar uma tarefa e executar
+        no = cabeca->prox;
+        aux = no->prox;
+        cabeca->prox = aux;
+        printf("Executando: %c\t%d\n", no->comando, no->tempo);
+        do_stuff(no->tempo);  //processa a tarefa
+        pthread_mutex_unlock(&lock_trabalhador);
+        printf("Executou tarefa\n");
+        pthread_cond_signal(&cond_mestre);
+    }
+}
+
+struct tarefa* le_tarefa(int i){
+    char *cmd, *tmp;
+    struct tarefa *no = (struct tarefa*) malloc(sizeof(struct tarefa));
+    cmd = strtok(buffer[i], " ");
+    tmp = strtok(NULL, " ");
+    no->comando = *cmd;
+    no->id = i;
+    no->tempo = atoi(tmp);
+    no->prox = NULL;
+    printf("Leu tarefa %c\t%d\n", no->comando, no->tempo);
+    return no;
+}
+
+
+/* FAZ AS ATRIBUICOES PARA AS VARIAVEIS GLOBAIS */
+void do_stuff(int tempo){
+    soma = soma + tempo;
+    if(tempo % 2 != 0){
+        impar += 1;
+    }
+    if(tempo < minimo){
+        minimo = tempo;
+    }
+    if(tempo > maximo){
+        maximo = tempo;
+    }
+    printf("%ld %ld %ld %ld\n", soma, impar, minimo, maximo);
+}
+
+/* INSERE NOVA TAREFA NA LISTA */
+int adiciona(struct tarefa *no){
+    struct tarefa *aux, *anterior;
+    anterior = cabeca;
+    aux = cabeca->prox;
+    while(aux != NULL){
+        anterior = aux;
+        aux = aux->prox;
+    }
+    aux = no;
+    anterior->prox = aux;
+    printf("Adicionou nova tarefa %d %c %d\n", aux->id, aux->comando, aux->tempo);
+}
+
+void mostra_tarefas(){
+    struct tarefa *aux;
+    printf("Mostra tarefas\n");
+    aux = cabeca->prox;
+    while(aux != NULL){
+        printf("TAREFA %d %c %d\n", aux->id, aux->comando, aux->tempo);
+        aux = aux->prox;
+    }
+}
+
 /* CALCULA O TAMANHO DO ARQUIVO LIDO */
 int calcula_tamanho_arq(FILE* arq){
     char c;
@@ -165,31 +294,13 @@ void inicializa_buffer(){
     }
 }
 
-/* CODIGO PARALELO */
-static void *mestre_trabalhador(void *arg){
-    thread_info *tinfo = arg;
-    if(!tinfo->ocioso){
-        if(tinfo->thread_num == MESTRE){
-            le_buffer();
-        }
+void remove_comando(char comando){
+    struct tarefa *aux, *anterior;
+    aux = cabeca;
+    while(aux->comando != comando){
+        anterior = aux;
+        aux = aux->prox;
     }
-}
-
-/* LE BUFFER PARA A FILA DE TAREFAS */
-int le_buffer(){
-    char *cmd, *tmp;
-    int i =  0;
-    do{
-        tarefa no;
-        cmd = strtok(buffer[i], " ");
-        tmp = strtok(NULL, " ");
-        strcpy(no.comando, cmd);
-        strcpy(no.tempo, tmp);
-
-        if(no.comando == PAUSA){
-            //parar leitura
-            break;
-        }
-        i+=1;
-    }while(1);
+    anterior->prox = aux->prox;
+    free(aux);
 }
